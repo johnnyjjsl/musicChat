825ad5cd5fc0b467d69d154f87ded39397ffb8ab
Johnny Sanchez <johnnysanchez730@icloud.com>
2023-09-11 21:18:08 -0400

    Initial Commit

---
+++ musicChat/musicChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  musicChatApp.swift
+//  musicChat
+//
+//  Created by Johnny Sanchez on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct musicChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
